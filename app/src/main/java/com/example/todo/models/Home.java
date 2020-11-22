8e1165898df2d568f85fe4a6b59c2a6ea51ca1e5
Caryl <caryl830@gmail.com>
2020-11-23 01:01:33 +0800

    Add database reading method in Home.java

--- app/src/main/java/com/example/todo/models/Home.java
+++ app/src/main/java/com/example/todo/models/Home.java
@@ -1,10 +1,46 @@
 package com.example.todo.models;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
 import com.example.todo.dbhelper.DatabaseHelper;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Home {
+    protected static final String TAG = "Home";
     private List<Task> goals;
     private double weeklyProgressPercentage;
+    private DatabaseHelper dbHelper;
+    private final Context context;
+
+    public Home(Context context) {
+        this.context = context;
+        dbHelper = new DatabaseHelper(context);
+    }
+
+    public List<Task> getGoals(Context context) throws IOException {
+        try {
+            String query = ""; //TODO: Update query
+
+            dbHelper.createDataBase();
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+            Cursor cursor = db.rawQuery(query, null);
+            cursor.moveToFirst();
+
+            while (!cursor.isAfterLast()) {
+
+            }
+
+            return goals;
+        } catch (IOException ex) {
+            Log.e(TAG, "getGoals >>" + ex.toString());
+            throw ex;
+        }
+    }
+
 }
