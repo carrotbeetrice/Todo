306a81aa72e2f7b16708066f9a2dcf15d4a02b73
sherrylee1534 <sherrylee1534@yahoo.com.sg>
2020-11-29 04:52:47 +0800

    getStartedScreenUI

---
+++ app/src/main/java/com/example/todo/models/GetStarted.java
@@ -0,0 +1,14 @@
+//package com.example.todo.models;
+//
+//import android.os.Bundle;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class GetStarted extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_welcome);
+//    }
+//}
