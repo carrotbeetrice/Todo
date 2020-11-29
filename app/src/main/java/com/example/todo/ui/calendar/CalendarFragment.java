4a4ececa033484d201a9e5e7bdf7e8495b91a28e
Sky Lee <jetxuen_lee@mymail.sutd.edu.sg>
2020-11-29 20:48:34 +0800

    Editted Calendar_Fragment.XML

--- app/src/main/java/com/example/todo/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/todo/ui/calendar/CalendarFragment.java
@@ -1,10 +1,14 @@
 package com.example.todo.ui.calendar;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.TextView;
+
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -32,4 +36,6 @@ public class CalendarFragment extends Fragment {
         });
         return root;
     }
+
+
 }
